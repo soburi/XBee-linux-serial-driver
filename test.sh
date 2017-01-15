01040e48df1391c573768a3a8bb07c701e9436fd
soburi <soburi@gmail.com>
2017-01-15 15:35:17 +0900

    test

---
+++ test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -x
+
+echo -n 'module xbee2 +p' > /sys/kernel/debug/dynamic_debug/control
+
+/usr/bin/killall -9 /usr/sbin/ldattach
+sleep 1
+ps -ef | grep ldattach
+/sbin/rmmod xbee2
+/sbin/rmmod mac802154
+/sbin/modprobe mac802154
+/sbin/insmod ./xbee2.ko
+sleep 1
+/usr/sbin/ldattach -s 9600 -8 -n -1 25 $1
