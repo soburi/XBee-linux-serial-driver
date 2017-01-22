e165d9ef9c579b65ceb92608375157b90acf276f
soburi <soburi@gmail.com>
2017-01-22 13:51:49 +0900

    correctly ./test.sh runs

--- test.sh
+++ test.sh
@@ -8,8 +8,8 @@ echo -n 'module xbee2 +p' > /sys/kernel/debug/dynamic_debug/control
 sleep 1
 ps -ef | grep ldattach
 /sbin/rmmod xbee2
-/sbin/rmmod mac802154
-/sbin/modprobe mac802154
+#/sbin/rmmod ieee802154
+/sbin/modprobe ieee802154
 /sbin/insmod ./xbee2.ko
 sleep 1
 /usr/sbin/ldattach -s 9600 -8 -n -1 25 $1
